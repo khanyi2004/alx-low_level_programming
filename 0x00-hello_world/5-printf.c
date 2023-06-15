922bd5726a81b88a924185a0585c426f61755603
khanyi2004 <khanyi.ramo@gmail.com>
2023-06-15 15:47:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>                                                                  
 /**                                                                                 
- *  main - Prints out a string to stdout.                            
- *  Return: 0 if success.                                                        
+ * main - Prints out a string to stdout.                            
+ * Return: (success)                                                        
 */                                                                                  
 int main(void)                                                                      
 {                                                                                   
