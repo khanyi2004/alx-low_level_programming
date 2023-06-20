13add7bd6703bb9f603c497f983200be3aaa2984
khanyi2004 <khanyi.ramo@gmail.com>
2023-06-20 12:46:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,36 +3,23 @@
 /**
 
  * main - print _putchar
-
- * is written by mbah
-
- * Return: 0
-
+ *
+ * is written by khanyi
+ *
+ * Return: 0 
  */
-
 
 int main(void)
 
 {
-
-_putchar('_');
-
-_putchar('p');
-
-_putchar('u');
-
-_putchar('t');
-
-_putchar('c');
-
-_putchar('h');
-
-_putchar('a');
-
-_putchar('r');
-
-_putchar('\n');
-
-return (0);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
