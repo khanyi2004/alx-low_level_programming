00850bba528bea7ec3aef38f3f51fcc7c8117db6
khanyi2004 <khanyi.ramo@gmail.com>
2023-06-20 13:47:44 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
-
- * main - print _putchar
+ * main - Prints "_putchar" followed by a new line.
  *
- * is written by khanyi
- *
- * Return: 0 
+ * Return: Always 0.
  */
-
 int main(void)
-
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
