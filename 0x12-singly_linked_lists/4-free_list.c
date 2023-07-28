af0220dff02a3e18d2babbe0f0825ea0a3540e0c
khanyi2004 <khanyi.ramo@gmail.com>
2023-07-28 16:39:12 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,23 @@
  * free_list - a linked list
  * @head: list_t list to be freed
  */
+#include "lists.h"
+/**
+  *free_list - main function
+  *@head: the head list
+  */
 void free_list(list_t *head)
 {
-	list_t *temp;
-
+	list_t *ptr;
+	if (!head)
+		return;
 	while (head)
-	{
-		temp = head->next;
+{
+	ptr = head->next;
+	if (head->str)
 		free(head->str);
-		free(head);
-		head = temp;
-	}
+	free(head);
+	head = ptr;
+}
+ free(ptr);
 }
